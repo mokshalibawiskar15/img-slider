00bab647c43e4a35abae1b9c6b07e1b8130fdbd4
Mokshali Bawiskar <45119091+mokshalibawiskar15@users.noreply.github.com>
2021-10-11 10:43:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+ 
+var arr = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg'];
+
+var i = 0;
+
+next.addEventListener('click',function(){
+    i++;
+    if(i>arr.length - 1){
+        i= 0;
+    }
+    document.getElementById('image').src = arr[i];
+
+});
+prev.addEventListener('click',function(){
+    i--;
+    if(i< 0){
+        i= arr.length - 1;
+    }
+    document.getElementById('image').src = arr[i];
+
+});
